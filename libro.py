5a9cc9fbf8a5fbf24bb208d4d48077e92009d473
RobertaMacaluso <48255899+RobertaMacaluso@users.noreply.github.com>
2025-10-08 10:34:58 +0200

    Add files via upload

---
+++ libro.py
@@ -0,0 +1,6 @@
+class Libro:
+    def __init__(self, titolo, autore, anno, pagine):
+        self.titolo = titolo
+        self.autore = autore
+        self.anno = anno
+        self.pagine = pagine
